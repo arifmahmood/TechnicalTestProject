5e61bdfcaa689fed13afa52454cf1f55d964a280
Arif <arifmahmoodsourav@gmail.com>
2021-01-30 16:18:11 +0600

    changing class path

--- pathaotechnicaltest/src/main/java/com/example/pathaotechnicaltest/service/FBInitialize.java
+++ pathaotechnicaltest/src/main/java/com/example/pathaotechnicaltest/service/FBInitialize.java
@@ -4,8 +4,10 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ResourceUtils;
 
 import javax.annotation.PostConstruct;
+import java.io.File;
 import java.io.FileInputStream;
 
 @Service
@@ -13,7 +15,9 @@ public class FBInitialize {
     @PostConstruct
     public void initialize() {
         try {
-            FileInputStream serviceAccount = new FileInputStream("D:\\pathao\\pathaotechnicaltest\\src\\main\\resources\\pathaotechnicaltest-firebase-adminsdk-rijea-bd42125785.json");
+            File file = ResourceUtils.getFile("classpath:pathaotechnicaltest-firebase-adminsdk-rijea-bd42125785.json");
+
+            FileInputStream serviceAccount = new FileInputStream(file);
             FirebaseOptions options = new FirebaseOptions.Builder()
                     .setCredentials(GoogleCredentials.fromStream(serviceAccount))
                     .setDatabaseUrl("https://chatapp-e6e15.firebaseio.com")
